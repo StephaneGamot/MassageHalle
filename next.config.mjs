210c81b11bc8ad35036201a405c755d22d229e4d
Stéphane Gamot <stephanegamot@outlook.com>
2025-12-01 16:15:41 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -222,6 +222,10 @@ const nextConfig = {
         source: '/%3A1',
         destination: '/',
         permanent: true,
+      }, {
+        source: '/:1',
+        destination: '/',
+        permanent: true,
       }
       
     ];
