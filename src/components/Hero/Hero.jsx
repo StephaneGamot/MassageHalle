f713f5183a54d37ebd984fdd73bcf20cce6584f1
Stéphane Gamot <stephanegamot@outlook.com>
2026-03-07 19:27:47 +0100

    re whats'app

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -71,7 +71,7 @@ export default function Hero() {
               href="https://wa.me/32477131993"
               target="_blank"
               rel="noreferrer"
-                    className="rounded-md bg-[#1EBE5D] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#8FBC8F] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#556B2F]"
+                    className="rounded-md bg-[#1EBE5D] px-9 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#8FBC8F] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#556B2F]"
                   >
                      WhatsApp
             </a>
